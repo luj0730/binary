fa2205b9b3b4ec5b20bc762be2725aa9ea7207c9
sv4us <2575954011@qq.com>
2019-11-24 08:56:29 +0800

    fix

--- centos6/build2.sh
+++ centos6/build2.sh
@@ -2,4 +2,4 @@ chmod 777 *
 
 #docker build -t centos:v1 . -f Dockerfile.v1
 docker build --no-cache -t centos/php56:v1 . -f Dockerfile.v2
-docker run --rm -it -p8080:80 -p3306:3306 centos/php56:v1 /usr/bin/zsh
+docker run --rm -it -p8080:80 -p3306:3306 centos/php56:v1 /bin/zsh
